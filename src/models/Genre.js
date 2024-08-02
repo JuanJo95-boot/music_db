613fa6eac1a3973be2c6f17a77259689e0516c82
David Vanegas <dvanegasf2016@gmail.com>
2024-08-01 22:17:49 -0500

    genre-con-rou-mod

--- src/models/Genre.js
+++ src/models/Genre.js
@@ -1,1 +1,11 @@
-//David
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection');
+
+const Genre = sequelize.define('Genre', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+});
+
+module.exports = Genre;
