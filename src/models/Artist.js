8a720a222dcad05d0fb3df98647327622acc47c6
JUDAGAPE7 <juandagacha@hotmail.com>
2024-08-01 22:35:18 -0500

    first artist commit

--- src/models/Artist.js
+++ src/models/Artist.js
@@ -1,1 +1,25 @@
 //Juan David
+
+const { DataTypes } = require("sequelize");
+const sequelize = require("../utils/connection");
+
+const Artist = sequelize.define("artists", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  country: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  formationYear: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
+
+module.exports = Artist;
