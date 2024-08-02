c725b57b7ae97a3f3ef10fe5080bda4c3713a315
gaby28894178 <gabgabrielligabriel@gmail.com>
2024-08-02 00:23:31 -0300

    Soung de gabrielli

--- src/controllers/song.controllers.js
+++ src/controllers/song.controllers.js
@@ -1,1 +1,44 @@
 //Gabriel
+const catchError = require('../utils/catchError');
+const Soungs = require('../models/Soungs');
+
+// Get All
+const getAll = catchError(async (req, res) => {
+    const result = await Soungs.findAll();
+    return res.json(result);
+});
+
+// Get One
+const getOne = catchError(async (req, res) => {
+    const { id } = req.params;
+    const result = await Soungs.findByPk(id);
+    return !result ? res.status(404).json({ msj: 'Nada q Mostrar' }) : res.json(result);
+});
+
+// POST
+const create = catchError(async (req, res) => {
+    const result = await Soungs.create(req.body);
+    return res.status(201).json(result);
+});
+
+// UPDATE
+const update = catchError(async (req, res) => {
+    const { id } = req.params;
+    const result = await Soungs.update(req.body, { where: { id }, returning: true });
+    return result[0] === 0 ? res.sendStatus(404) : res.json(result[1][0]);
+});
+
+// DELETE
+const destroy = catchError(async (req, res) => {
+    const { id } = req.params;
+    const result = await Soungs.destroy({ where: { id } });
+    return !result ? res.status(404).json({ msj: 'Nada Para mostrar' }) : res.status(204).json({ msg: 'Dato Eliminado con Exito' });
+});
+
+module.exports = {
+    getAll,
+    getOne,
+    create,
+    update,
+    destroy
+};
--- src/routes/song.router.js
+++ src/routes/song.router.js
@@ -1,1 +1,18 @@
 //Gabriel
+const { getAll, getOne, create, update, destroy } = require('../controllers/soungs.controllers');
+//  getAll,
+
+const express = require('express');
+
+const soungsRouter = express.Router();
+
+soungsRouter.route('/')
+    .get(getAll)
+    .post(create);
+
+soungsRouter.route('/:id')
+    .get(getOne)
+    .delete(destroy)
+    .put(update);
+
+module.exports = soungsRouter;
