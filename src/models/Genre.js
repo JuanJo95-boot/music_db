d88e2955456a5a4ace03647f7375eed22101a3d7
Juan Yidi <juanyiche@gmail.com>
2024-08-02 19:28:59 -0500

    fifth-commit

--- src/models/Genre.js
+++ src/models/Genre.js
@@ -1,11 +1,11 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../utils/connection');
 
-const Genre = sequelize.define('Genre', {
+const Genre = sequelize.define('genre', {
     name: {
         type: DataTypes.STRING,
         allowNull: false
-    },
+    }
 });
 
 module.exports = Genre;
